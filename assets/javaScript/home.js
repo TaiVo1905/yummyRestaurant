b9c3823f5cf3aa2e125bc69f9dd66acf0cd96fe7
hoviettien <tien.ho26@student.passerellesnumeriques.org>
2024-10-12 23:31:27 +0700

    ...

--- assets/javaScript/home.js
+++ assets/javaScript/home.js
@@ -1,21 +1,23 @@
-fetch('../assets/Data/data.json')
-    .then(response => response.json()) //khi fetch dữ liệu nó sẽ trở thành RESPONSE nên phải chuyển thành JSON
-    .then(data => { //chứa JSON đã được chuyển đổi
-        const menuAllDish = document.getElementById('body-menu'); //nơi để đưa thông tin các món ăn vào
-        data.featuredDishes.forEach(dish => {
-            const cardDish = document.createElement('createDiv'); //tạo div mẫu
-            cardDish.className = 'menu_card'; //đặt tên cho card này để ăn css
+function renderFeaturedDishes(){
+    fetch('../assets/Data/data.json')
+        .then(response => response.json()) //khi fetch dữ liệu nó sẽ trở thành RESPONSE -> chuyển thành object JS
+        .then(data => { //return data đại diện cho đối  tượng được trả về
+            const menuAllDish = document.getElementById('body-menu'); //nơi để đưa thông tin các món ăn vào
+                data.featuredDishes.forEach(dish => { //duyệt từng phần tử trong featuredDishes
+                    menuAllDish.innerHTML += `
+                        <div class="menu_card">
+                            <div class="card_dish">
+                                <img src="${dish.image_url}">
+                            </div>
+                            <h5>${dish.name}</h5>
+                            <h6>${dish.price}</h6>
+                            <button>Thêm vào giỏ hàng</button>
+                        </div>
+                    `;
+                });
+        })
+}
 
-            cardDish.innerHTML = `
-                <div class="card_dish">
-                    <img src="${dish.image_url}">
-                </div>
-                <h5>${dish.name}</h5>
-                <h6>${dish.price}</h6>
-                <button>Thêm vào giỏ hàng</button>
-            `;
-
-            menuAllDish.appendChild(cardDish); //thêm cardDish vào menuAllDish
-        });
-    })
-    // .catch(error => console.error('Error loading menu:', error)); CHƯA TÌM HIỂU (Nếu có lỗi trong quá trình tải hoặc xử lý dữ liệu, phần này sẽ bắt lỗi và in ra console với thông báo.)
+renderFeaturedDishes();
+        
+    
