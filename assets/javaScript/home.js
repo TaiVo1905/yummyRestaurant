df064671db49ad5d0480c8262762b49079dff802
bnuoch thi thuy <bnuochthithuy13032005@gmail.com>
2024-10-12 20:32:28 +0700

    ...

--- assets/javaScript/home.js
+++ assets/javaScript/home.js
@@ -1,11 +1,10 @@
 fetch('../assets/Data/data.json')
     .then(response => response.json()) //khi fetch dữ liệu nó sẽ trở thành RESPONSE nên phải chuyển thành JSON
-    .then(data => { //chứa JSON đã được chuyển đổi
+    .then(data => { //chứa JSON đã được chuyển đổi thành đối tượng
         const menuAllDish = document.getElementById('body-menu'); //nơi để đưa thông tin các món ăn vào
         data.featuredDishes.forEach(dish => {
             const cardDish = document.createElement('createDiv'); //tạo div mẫu
             cardDish.className = 'menu_card'; //đặt tên cho card này để ăn css
-
             cardDish.innerHTML = `
                 <div class="card_dish">
                     <img src="${dish.image_url}">
@@ -14,12 +13,10 @@ fetch('../assets/Data/data.json')
                 <h6>${dish.price}</h6>
                 <button>Thêm vào giỏ hàng</button>
             `;
-
             menuAllDish.appendChild(cardDish); //thêm cardDish vào menuAllDish
         });
     })
     // .catch(error => console.error('Error loading menu:', error)); CHƯA TÌM HIỂU (Nếu có lỗi trong quá trình tải hoặc xử lý dữ liệu, phần này sẽ bắt lỗi và in ra console với thông báo.)
-
     function handleLogAndRegModal() {
         const logAndReg = document.getElementById("logAndReg");
         const logAndReg_modal = document.getElementById("logAndReg_modal");
@@ -30,7 +27,6 @@ fetch('../assets/Data/data.json')
             if(e.target === logAndReg_modal){
                 logAndReg_modal.style.display = 'none';
             }
-
         })
     }
     handleLogAndRegModal();
