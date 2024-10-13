d043474a61b92dc890353adb3732c22226146d4f
ChăngSeven2k5 <changcoor7@gmail.com>
2024-10-13 12:00:35 +0700

    fixed

--- assets/javaScript/cart.js
+++ assets/javaScript/cart.js
@@ -38,13 +38,23 @@ function checkIfCartIsEmpty() {
 // hàm xoá sản phẩm khỏi giỏ hàng
 function removeCartItem(event) {
     const buttonClicked = event.target;
-    buttonClicked.closest('tr').remove();
+    const cartRow = buttonClicked.closest('tr'); // Lấy dòng sản phẩm (row)
+    const productName = cartRow.querySelector('.product_name a').innerText; // Lấy tên sản phẩm
 
+
+    // Xóa sản phẩm khỏi localStorage
+    const updatedCartLocalStorage = data.carts.filter(cart => cart.nameFood !== productName);
+    data.carts = updatedCartLocalStorage;// Cập nhật data
+    setDataLocalStorage(data); //Cập nhật lại localStrorage
+
+
+    cartRow.remove();
     updateCartTotal();
     checkIfCartIsEmpty();
     // Hiển thị thông báo tùy chỉnh
     const notification = document.getElementById('delete_notification');
     notification.style.display = 'block';
+
     
     setTimeout(() => {
         notification.style.display = 'none';
@@ -89,4 +99,49 @@ document.addEventListener('DOMContentLoaded', function() {
     checkIfCartIsEmpty();
 });
 
+//////////////////////////////////////////////////////////////////////////////
+
+import getDataLocalStorage, {setDataLocalStorage} from "../javaScript/localStorage.js";
+const data = getDataLocalStorage()
+
+
+function displayCart(data){
+     // Truy xuất đến phần tử có ID 'menu_body'
+     const cartTable = document.getElementById('menu_body');
+     cartTable.innerHTML = ""; // Xóa các sản phẩm hiện tại trong giỏ hàng
+
+    data.carts.forEach(cart => {
+    // Nội dung HTML của từng dòng sản phẩm
+    cartTable.innerHTML += `
+                <tr class="cart_form_products">
+                      <!-- Xoá sản phẩm đã thêm vào giỏ hàng -->
+                      <td class="product_remove">
+                          <a href="#" class="remove"><i class="fa-regular fa-circle-xmark item_remove"></i></a>
+                      </td>
+                      <td class="product_image appear">
+                          <a href="details.html" class="image"><img src="${cart.image_url}" style="width: 80px; height: 80px;" alt="${cart.nameFood}"></a>
+                      </td>
+                      <td class="product_name appear">
+                          <a href="details.html">${cart.nameFood}</a>
+                      </td>
+                      <td class="product_price appear">
+                          <a href="details.html">${cart.price}</a>				
+                      </td>
+                      <!-- Tăng giảm số lượng đơn hàng -->
+                      <td class="product_quantity">
+                          <div class="quantity buttons_added">
+                              <input type="button" onclick="this.parentNode.querySelector('input[type=number]').stepDown()" id="number_subtraction" value="-">
+                              <input type="number" value="1" id="number_step" min="1">
+                              <input type="button" onclick="this.parentNode.querySelector('input[type=number]').stepUp()" id="number_addition" value="+">
+              
+                          </div>
+                      </td>
+                      <td class="product_subtotal appear">${cart.price}				
+                      </td>
+                </tr>
+      `;
+    });
+}
+
+displayCart(data)
 
