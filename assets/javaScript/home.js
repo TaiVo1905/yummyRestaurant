e028019547257d8552ffe7b61c3365d55de59f04
Vo Duc Tai <voductaitxqt123@gmail.com>
2024-10-20 20:59:38 +0700

    ...

--- assets/javaScript/home.js
+++ assets/javaScript/home.js
@@ -82,6 +82,6 @@ function switchToDetailPage() {
 renderFeaturedDishes(allData);
 handleCart();
 handleLogAndRegModal();
-import countUniqueItemsInCart, from "../javaScript/cart.js";
+// import countUniqueItemsInCart from "../javaScript/cart.js";
 switchToDetailPage();
 
