d7d1b7acb83391fffa29d6dfcfd10c4f9f7dd352
TaiVo1905 <voductaitxqt123@gmail.com>
2024-10-23 12:15:41 +0700

    Update app.js

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -91,7 +91,7 @@ export function toast (message) {
 // Hàm chạy chương trình
 function runPage(data) {
     document.addEventListener('DOMContentLoaded', () => {
-        if (window.location.pathname == '/index.html' || window.location.pathname == '/ymmyRestaurantWebsite/index.html' || window.location.pathname == '/ymmyRestaurantWebsite/' || window.location.pathname == '/') {
+        if (window.location.pathname == '/index.html' || window.location.pathname == '/yummyRestaurantWebsite/index.html' || window.location.pathname == '/yummyRestaurantWebsite/' || window.location.pathname == '/') {
             renderFeaturedDishes(data);
             handleCart(data);
             switchToDetailPage();
